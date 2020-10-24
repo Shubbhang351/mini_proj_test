ffcceee8a93beca64245ff850d96dd86c00c57a6
Shubbhang <shubhang.verma_cs18@gla.ac.in>
2020-10-24 22:59:52 +0530

    initial commit

--- api/server.js
+++ api/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
-
+const path = require('path');
 const server = express();
  
 server.use(express.json());
+
+// EXPRESS SPECIFIC STUFF
+server.use('/static',express.static('static')); // serving static files
+server.use(express.urlencoded());
+
+server.set('views', path.join(__dirname, 'templates')); //set the templates directory
 
 
 server.get('/', (req, res) => { 
